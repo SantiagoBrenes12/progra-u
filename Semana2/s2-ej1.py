419a9d89123c3a9d7a697824d923a6d02b45a404
SantiagoBrene12 <itssanti_@outlook.com>
2022-09-22 23:01:07 -0600

    Adding w2 work

---
+++ Semana2/s2-ej1.py
@@ -0,0 +1,85 @@
+
+# Ejercicio 1: Orden inverso
+a = 1
+b = 2 
+c = 3
+d = 4
+
+print(d,c,b,a, sep='\n')
+
+
+#Ejercicio 2: IVA a monto
+IVA = 0.13
+monto = 20000
+
+print(f'El IVA de {monto} es {monto*IVA}')
+
+
+#Ejercicio 3:
+"""
+ Elabore un programa que solicite al usuario la edad y calcule cuántos años 
+    tendrá la persona en 5 años, al finalizar se imprime el siguiente mensaje 
+    “Dentro de 5 años, tendrá:” y se muestra el valor de la edad proyectada
+"""
+
+# nombre = input('Porfavor digite su nombre: ')
+# edad = int(input('Porfavor digite su edad: '))
+# edad_final = edad+5
+
+# print(f'{nombre} Usted dentro de 5 años, tendrá: {edad_final} años')
+
+
+#Ejercicio 4:
+"""
+Construya un programa que le solicite al usuario
+los datos enteros A y B y muestre el resultado de la siguiente expresión : a+b**2 / 3
+"""
+
+# a = int(input('Digite un numero'))
+# b = int(input('Digite otro numero'))
+# resultado = ((a+b)**2) // 3
+
+# print(f'El resultado es: {resultado}')
+
+#Ejercicio 5:
+"""
+ Desarrolle un programa que le solicite un número al usuario y calcule el 
+ cuadrado y el cubo de este.
+"""
+
+# numero = int(input('Digite un numero'))
+# resultado_cuadrado = numero**2
+# resultado_cubo = numero**3
+
+# print(f'El resultado al cuadrado es: {resultado_cuadrado}\nEl resultado al cubo es: {resultado_cubo}')
+
+#Ejercicio 6:
+"""
+ Realice un programa que, dada la base y la altura 
+ de un rectángulo, calcule el área y el perímetro de este.
+"""
+
+# base = int(input('Digite la base: '))
+# altura = int(input('Digite la altura: '))
+# ancho = int(input('Digite el ancho: '))
+# alto = int(input('Digite el alto: '))
+
+# area = base*altura
+# perimetro = ancho*2 + alto*2
+
+# print(f'El area de su rectangulo es: {area}\nSu perimetro es {perimetro}')
+
+
+#Ejercicio 7:
+"""
+    Desarrolle un programa que solicite la distancia de su casa a la
+    Universidad, el costo por kilómetro, la cantidad de días a la semana que 
+    viaja a la Universidad y que calcule el costo total de trasladarse por
+    cuatrimestre. Asuma que cada visite implica ida y vuelta y que el 
+    cuatrimestre tiene 15 semanas
+"""
+
+distancia = int(input('Digite la distancia de su casa a la universidad en km'))
+costo_por_km = int(input('Di'))
+distancia = int(input('Digite la distancia de su casa a la universidad en km'))
+
