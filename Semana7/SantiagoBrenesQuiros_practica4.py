4f6b1171cfb62c5aaef2c5e08950e26920a10e75
SantiagoBrene12 <itssanti_@outlook.com>
2022-10-23 23:35:46 -0600

    Practica #4

--- Semana7/SantiagoBrenesQuiros_practica4.py
+++ Semana7/SantiagoBrenesQuiros_practica4.py
@@ -6,19 +6,11 @@ numero = int(input("Ingrese un numero entre 1 y 20: "))
 fizbuzz = ""
 
 for i in range(1, numero + 1):
-    # if (i % 3) == 0 and (i % 5) == 0:
-    #     print("3,5", i)
-    #     fizbuzz += "fizzbuzz"
-    #     print(fizbuzz)
+    if (i % 3) == 0 and (i % 5) == 0:
+        fizbuzz += "fizzbuzz"
     if i % 3 == 0:
-        print("3", i)
         fizbuzz += "fizz"
-        print(fizbuzz)
-
     if i % 5 == 0:
-        print("5", i)
         fizbuzz += "buzz"
-        print(fizbuzz)
 
-
-print("fizzbuzzfizzfizzbuzzfizzfizzbuzz" == "fizzbuzzfizzfizzbuzzfizzfizzbuzz")
+print(fizbuzz)
